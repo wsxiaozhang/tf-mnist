b9e6359f55b03ef27f9fc05c3dc7d1d5d710974f
ali-131586N <ali-131586n@ali-4c32758b20db.local>
2017-03-06 17:48:03 +0800

    add dockerfile for local train with git src code

---
+++ train_with_git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Cloning training code from ", $GIT_URL
+git clone $GIT_URL
+echo "Clone code done."
+
+echo "Run training code as: ", "$@"
+
+exec "$@"
+
+echo "Run training done."
