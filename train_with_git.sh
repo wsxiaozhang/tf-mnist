72727d1fbe7cafbc5b05318520fdaaef8f13c6a1
ali-131586N <ali-131586n@ali-4c32758b20db.local>
2017-03-07 12:41:36 +0800

    correct starter script

--- train_with_git.sh
+++ train_with_git.sh
@@ -4,21 +4,21 @@ default_ckpt_path='/tmp/train-log/checkpoint'
 
 echo "Cloning training code from " $GIT_URL
 git clone $GIT_URL
-echo "Clone code done."
+echo "Done clone code."
 
 echo "Run training code as: " $@
 
 eval "$@"
 
-echo "Run training done."
+echo "Done running training code."
 
 ckpt_local_path=$CKPT_LOCAL_PATH
 ckpt_remote_path=$CKPT_OSS_PATH
 
-echo 'Saving checkpoints from' $ckpt_local_path 'to ' $ckpt_remote_path
+echo 'Persist checkpoints from' $ckpt_local_path 'to ' $ckpt_remote_path
 
 cp -r $ckpt_local_path $ckpt_remote_path
 
 ls -l $ckpt_remote_path
 
-echo "Save checkpoints to remote storage done."
+echo "Done persisting checkpoints to remote storage."
