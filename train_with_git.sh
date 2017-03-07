5add127da0a82096812b6c14836afb978a68f0b1
ali-131586N <ali-131586n@ali-4c32758b20db.local>
2017-03-07 12:04:12 +0800

    update train starter script

--- train_with_git.sh
+++ train_with_git.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-echo "Cloning training code from " $GIT_URL
+default_ckpt_path='/tmp/train-log/checkpoint'
+
+echo "Cloning training code from ", $GIT_URL
 git clone $GIT_URL
 echo "Clone code done."
 
-echo "Run training code as: $@"
+echo "Run training code as: ", "$@"
 
-exec "$@"
+eval "$@"
 
 echo "Run training done."
+
+ckpt_local_path=$CKPT_LOCAL_PATH
+ckpt_remote_path=$CKPT_OSS_PATH
+
+echo 'Saving checkpoints from' $ckpt_local_path 'to ' $ckpt_remote_path
+
+cp -r $ckpt_local_path $ckpt_remote_path
+
+ls -l $ckpt_remote_path
+
+echo "Save checkpoints to remote storage done."
