4ecc7b5a5d002502b8b8ee8eec03972f485cf33e
ali-131586N <ali-131586n@ali-4c32758b20db.local>
2017-03-21 19:31:12 +0800

    update train_with_git scripts to create default input output dir in container

--- train_with_git.sh
+++ train_with_git.sh
@@ -2,12 +2,16 @@
 
 #default_ckpt_path='/output/train-log/checkpoint'
 default_output_path='/output'
-default_oss_basePath='/var/oss/'
+default_oss_volume_path=$DEFAULT_REMOTE_VOLUME_PATH
 default_input_path='/input'
 
 echo "Cloning training code from " $GIT_URL
 git clone $GIT_URL
 echo "Done clone code."
+
+if [ -d "$default_oss_volume_path" ]; then
+  ln -s $default_oss_volume_path $default_input_path
+fi
 
 if [ ! -d "$default_output_path" ]; then
   mkdir $default_output_path
@@ -20,8 +24,8 @@ eval "$@"
 echo "Done running training code."
 
 ckpt_local_path=$default_output_path
-#ckpt_remote_path=$default_oss_path + $REMOTE_VOLUME_PATH
-ckpt_remote_path=$default_input_path
+ckpt_remote_path=$DEFAULT_REMOTE_VOLUME_PATH
+#ckpt_remote_path=$default_input_path
 
 echo 'Persist checkpoints from' $ckpt_local_path 'to ' $ckpt_remote_path
 
